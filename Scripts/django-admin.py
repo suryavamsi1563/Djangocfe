a0de810fc87ba27fe4db0f6bbf3fe84a455710bb
surya vamsi <suryavamsi1563@gmail.com>
2018-05-23 00:03:32 +0530

    Added the 1.11.2 project of trydjango from codeforentrepreneurs.

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suryavamsi\desktop\django\dev\trydjango1-11\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
